534b17effdf0e698ffb3faabb465efa83b5076ff
iLodhi <143966478+ilodhi@users.noreply.github.com>
2025-03-22 11:42:29 -0800

    Site updated: 2025-03-22 11:42:28

--- admin/custom/modern-admin.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-// Modern UI enhancements
-document.addEventListener("DOMContentLoaded", () => {
-	// Replace default editor with CodeMirror or Monaco Editor
-	// Add modern UI components
-});
